cd49cedf402b30465bf3c730884af5810865b591
shirinyamani <yamani.shirin@ucalgary.ca>
2024-03-25 17:20:03 -0600

    array anagram added

--- DS/array/anagram.py
+++ DS/array/anagram.py
@@ -1,42 +1,23 @@
-import collections
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+    
+        if len(s) != len(t):
+            return False    
+        mapping = {}
+        for char in s:
+            if char not in mapping:
+                mapping[char] = 1
+            else:
+                mapping[char] += 1
 
-def anagram(s,t):
-    s = s.replace(' ','').lower()
-    t = t.replace(' ','').lower()
+        for char in t:
+            if char in mapping:
+                mapping[char] -= 1
+            else:
+                return False
+        for c in mapping.values():
+            if c != 0:
+                return False
+            
+        return True
 
-    return sorted(s) == sorted(t)
-
-def anagram2(s,p):
-    return collections.Counter(s) == collections.Counter(p)
-         
-
-
-def anagram3(s,p):
-    if len(s)!= len(p):
-        return False
-
-    mapping = {}
-    for char in s:
-        if char not in mapping:
-            mapping[char] = 1
-        else:
-            mapping[char] += 1
-
-    for char in p:
-        if char in mapping:
-            mapping[char] -= 1
-        else:
-            return False
-    for c in mapping.values():
-        if c != 0:
-            return False
-        
-    return True
-
-
-
-
-
-
-if __name__ == "__main__":
-    print(anagram3('shirin','nooshin'))
