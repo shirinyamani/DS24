c155ccbe3c8f52311a7a853d0e5ddd8f8349be9d
Shirin Yamani <yamani.shirin@ucalgary.ca>
2024-04-16 01:52:08 -0600

    search added

---
+++ DS/search/binary_search.py
@@ -0,0 +1,11 @@
+def search(self, nums: List[int], target: int) -> int:
+    l,r=0,len(nums)-1
+    while l<=r:
+        m=(l+r)//2
+        if nums[m]>target:
+            r=m-1
+        elif nums[m]<target:
+            l=m+1
+        else:
+            return m
+    return -1
