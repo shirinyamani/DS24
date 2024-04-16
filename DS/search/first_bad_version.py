6553305304f37941b5cbe6000d659dc537341758
Shirin Yamani <yamani.shirin@ucalgary.ca>
2024-04-16 11:56:56 -0600

    first bad version added

---
+++ DS/search/first_bad_version.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        if n == 0:
+            return None
+
+        l, r = 0, n-1
+        while l <= r:
+            mid = (l + r) // 2
+            if isBadVersion(mid):
+                r = mid - 1
+
+            else:
+                l = mid + 1
+        return l
